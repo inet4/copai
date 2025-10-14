8e51df003cdcbf95bbbf339e459c912c72662572
inet4 <chikcnmail@gmail.com>
2025-10-14 12:45:19 +0200

    feat: implement answering logic

--- src/background.js
+++ src/background.js
@@ -1,5 +1,18 @@
+
+const PROMPT = "You are a networking specialist, please give the correct answer to the following question. If it is in German, then answer in German. If there are options provided for the question, just return the index of the correct answer:";
+
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  fetch(request.answersURL)
+  fetch("https://api.openai.com/v1/responses", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": `Bearer ${request.apiKey}`
+    },
+    body: JSON.stringify({
+      model: "gpt-5-nano",
+      input: PROMPT + request.question
+    })
+  })
     .then((response) => response.text())
     .then((text) => {
       sendResponse({ html: text });
