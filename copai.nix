6cccfd35932754df51253cd018051231823fbe64
inet4 <chikcnmail@gmail.com>
2025-12-15 10:59:36 +0100

    chore: update flake version

--- copai.nix
+++ copai.nix
@@ -2,13 +2,13 @@
 
 buildFirefoxXpiAddon rec {
   pname = "copai";
-  version = "1.0.0";
+  version = "1.0.2";
   addonId = "copai@inet4.github.com";
   url = "https://github.com/inet4/copai/releases/download/v${version}/${pname}@inet4.github.com.xpi";
-  sha256 = "sha256-fJdwNM1mLVeUuP17p7bkG2Hg2aHyr4OSR6kqfEK4bTk=";
+  sha256 = "sha256-qh0aYiZZohbknE0Y4cFEdv75ijrJZgAKdXyRReqw/Uo=";
   meta = with lib; {
     homepage = "https://github.com/inet4/copai";
-    description = ":)";
+    description = "funny cat images!";
     license = licenses.mit;
     platforms = platforms.all;
   };
